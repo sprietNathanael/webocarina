064b9055fd69c3b5bd8b7f0038a602d02a1e47dc
Nathanaël SPRIET <nathanael.spriet@gmail.com>
2016-03-29 21:05:17 +0200

    Begin mere implementation of TypeOcarina

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 # -*-coding:UTF-8 -*
 
+from sqlalchemy.ext.declarative import declarative_base
+from typeOcarinaORM import TypeOcarinaORM
+
 
 class Main(object):
     """docstring for Main"""
     def __init__(self,):
+        self.typeOcarina_orm = TypeOcarinaORM()
         while True:
             self.afficherMenu()
             self.validerMenu()
 
     def afficherMenu(self):
         print("1. Bonjour")
-        print("2. Quitter")
+        print("2. Afficher tous les types d'ocarina")
+        print("255. Quitter")
 
     def quitter(self):
         print("Merci d'avoir utilisé notre programme !")
@@ -21,16 +26,19 @@ class Main(object):
         try:
             i = (input(invite))
         except KeyboardInterrupt:
-            print("\nPour quitter, faites le choix 2")
+            print("\nPour quitter, faites le choix 255")
         except EOFError:
-            print("\nPour quitter, faites le choix 2")
+            print("\nPour quitter, faites le choix 255")
         else:
             if i == "1":
                 print("Bonjour !\n")
             elif i == "2":
+                print(self.typeOcarina_orm.typeList())
+            elif i == "255":
                 self.quitter()
             else:
                 print("Votre choix est faux")
 
 if __name__ == '__main__':
     myMain = Main()
+
